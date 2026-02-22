dc7ab48ec29905b2c7176f39e2dfc97f923b23ec
Bruno Croci <bruno@croci.me>
2026-02-22 17:35:02 +0100

    feat: kicking off the cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create a new clutterlog site
+    New {
+        /// Name of the site to create
+        site_name: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::New { site_name } => {
+            println!("Creating new site: {}", site_name);
+        }
+    }
 }
